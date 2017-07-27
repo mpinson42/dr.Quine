1f84b046d6e5bdd1841252b1f5fe5d879cc63cf3
Mathieu PINSON <mpinson@e3r1p2.42.fr>
2017-07-27 13:30:39 +0200

    clear

--- include/dr.h
+++
@@ -1,8 +0,0 @@
-#ifndef DR_H
-# define DR_H
-
-
-#include "libft.h"
-#define PRINT "#include \"dr.h\"\n\n/*\n	non\n*/\n\nvoid ft_affiche(void)\n{\n	ft_putstr(PRINT);\n}\n\nint main()\n{\n	/*\n		oui.\n	*/\n	ft_affiche();\n}\n"
-
-#endif
